f8c4d8c1c20f7acaaca4b25b2ca1bf3f1b03c199
Sergey <31743075+serg-chs@users.noreply.github.com>
2018-06-02 14:37:56 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+package com.sergchs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> col=new ArrayList<>();
+            System.out.println("Здравствуйте "+args[0]);
+    }
+}
